2d196cf7eeb88c77d41dbe67ac904c6239439c72
zzw <602129484@qq.com>
2016-04-18 11:22:24 +0800

    后台定位需真机测试

---
+++ allowsBackgroundLocationUpdates/allowsBackgroundLocationUpdates/MyAnnotation.h
@@ -0,0 +1,13 @@
+//
+//  MyAnnotation.h
+//  allowsBackgroundLocationUpdates
+//
+//  Created by zzw on 16/4/18.
+//  Copyright © 2016年 zzw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyAnnotation : NSObject
+
+@end
---
+++ allowsBackgroundLocationUpdates/allowsBackgroundLocationUpdates/MyAnnotation.m
@@ -0,0 +1,13 @@
+//
+//  MyAnnotation.m
+//  allowsBackgroundLocationUpdates
+//
+//  Created by zzw on 16/4/18.
+//  Copyright © 2016年 zzw. All rights reserved.
+//
+
+#import "MyAnnotation.h"
+
+@implementation MyAnnotation
+
+@end
